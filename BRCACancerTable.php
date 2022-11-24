56a4da43a0446fcfe09499f2c585e786ceb6e6ff
Pedro Sánchez Luling <slpedro@hotmail.com>
2022-11-24 11:08:03 +0100

    LC2-9548 Support normalized ICD9 Codes

--- BRCACancerTable.php
+++ BRCACancerTable.php
@@ -1,4 +1,5 @@
 <?php
+
 class BRCACancerTable {
     const BREAST_CANCER = 'breast';
     const OVARIAN_CANCER = 'ovary';
@@ -6,19 +7,25 @@ class BRCACancerTable {
     const PROSTATE_CANCER = 'prostate';
     const COLON_CANCER = 'colon';
     private $_cancerTable;
+
     public function __construct() {
-        $this->_cancerTable['C50911'] = ['code' => 'C50911','description' => 'Female right breast','type' => BRCACancerTable::BREAST_CANCER,"lateral" => "right"];
-        $this->_cancerTable['C50912'] = ['code' => 'C50912','description' => 'Female left breast','type' => BRCACancerTable::BREAST_CANCER,"lateral" => "left"];
-        $this->_cancerTable['C569'] = ['code' => 'C569','description' => 'Ovary','type' => BRCACancerTable::OVARIAN_CANCER,"lateral" => "no"];
-        $this->_cancerTable['C50921'] = ['code' => 'C50921','description' => 'Male right breast','type' => BRCACancerTable::BREAST_CANCER,"lateral" => "right"];
-        $this->_cancerTable['C50922'] = ['code' => 'C50922','description' => 'Male left breast','type' => BRCACancerTable::BREAST_CANCER,"lateral" => "left"];
-        $this->_cancerTable['C61'] = ['code' => 'C61','description' => 'Prostate','type' => BRCACancerTable::PROSTATE_CANCER,"lateral" => "no"];
-        $this->_cancerTable['C259'] = ['code' => 'C259','description' => 'Pancreatic','type' => BRCACancerTable::PANCREATIC_CANCER,"lateral" => "no"];
-        $this->_cancerTable['C189'] = ['code' => 'C189','description' => 'Colon','type' => BRCACancerTable::COLON_CANCER,"lateral" => "no"];
+        $this->_cancerTable['C50911'] = ['code' => 'C50.911', 'description' => 'Female right breast', 'type' => BRCACancerTable::BREAST_CANCER,
+                "lateral" => "right"];
+        $this->_cancerTable['C50912'] = ['code' => 'C50.912', 'description' => 'Female left breast', 'type' => BRCACancerTable::BREAST_CANCER,
+                "lateral" => "left"];
+        $this->_cancerTable['C56900'] = ['code' => 'C56.9', 'description' => 'Ovary', 'type' => BRCACancerTable::OVARIAN_CANCER, "lateral" => "no"];
+        $this->_cancerTable['C50921'] = ['code' => 'C50.921', 'description' => 'Male right breast', 'type' => BRCACancerTable::BREAST_CANCER,
+                "lateral" => "right"];
+        $this->_cancerTable['C50922'] = ['code' => 'C50.922', 'description' => 'Male left breast', 'type' => BRCACancerTable::BREAST_CANCER,
+                "lateral" => "left"];
+        $this->_cancerTable['C61000'] = ['code' => 'C61', 'description' => 'Prostate', 'type' => BRCACancerTable::PROSTATE_CANCER, "lateral" => "no"];
+        $this->_cancerTable['C25900'] = ['code' => 'C25.9', 'description' => 'Pancreatic', 'type' => BRCACancerTable::PANCREATIC_CANCER,
+                "lateral" => "no"];
+        $this->_cancerTable['C18900'] = ['code' => 'C18.9', 'description' => 'Colon', 'type' => BRCACancerTable::COLON_CANCER, "lateral" => "no"];
     }
-    
+
     /**
-     * 
+     *
      * @return BRCACancerTable
      */
     public static function getInstance() {
@@ -26,19 +33,23 @@ class BRCACancerTable {
         if (null === $instance) {
             $instance = new static();
         }
-        
+
         return $instance;
     }
+
     public function getCancerType($code, &$lateral) {
+        $code = self::normalizeICDCode($code);
         $lateral = null;
-        if (! array_key_exists($code, $this->_cancerTable))
+        if (!array_key_exists($code, $this->_cancerTable))
             return null;
-        
+
         $lateral = $this->_cancerTable[$code]['lateral'];
         return $this->_cancerTable[$code]['type'];
     }
+
     public function isMale($code) {
-        if (! array_key_exists($code, $this->_cancerTable))
+        $code = self::normalizeICDCode($code);
+        if (!array_key_exists($code, $this->_cancerTable))
             return null;
         $desc = $this->_cancerTable[$code]['description'];
         if (strpos($desc, "Male") !== false) {
@@ -49,8 +60,10 @@ class BRCACancerTable {
         }
         return false;
     }
+
     public function isFemale($code) {
-        if (! array_key_exists($code, $this->_cancerTable))
+        $code = self::normalizeICDCode($code);
+        if (!array_key_exists($code, $this->_cancerTable))
             return null;
         $desc = $this->_cancerTable[$code]['description'];
         if (strpos($desc, "Female") !== false) {
@@ -61,10 +74,17 @@ class BRCACancerTable {
         }
         return false;
     }
+
     public function getCancerDescription($code) {
-        if (! array_key_exists($code, $this->_cancerTable))
+        $code = self::normalizeICDCode($code);
+        if (!array_key_exists($code, $this->_cancerTable))
             return "";
-        
+
         return $this->_cancerTable[$code]['description'];
+    }
+
+    static private function normalizeICDCode($code) {
+        $code = strtoupper(str_replace('.', '', $code));
+        return str_pad($code, 6, '0', STR_PAD_RIGHT);
     }
 }
